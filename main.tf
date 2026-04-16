ee5b3df890478e047d18768da8287a5b372f7606
Bathini Akhil charan <bathiniakhilcharan@Bathinis-MacBook-Pro.local>
2026-04-16 11:08:02 +0530

    adding the dev and stage/Prod

--- main.tf
+++ main.tf
@@ -2,16 +2,38 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  environments = {
+    dev = {
+      cidr             = "10.0.0.0/16"
+      private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+      public_subnets   = ["10.0.3.0/24", "10.0.4.0/24"]
+    }
+    stage = {
+      cidr             = "10.1.0.0/16"
+      private_subnets  = ["10.1.1.0/24", "10.1.2.0/24"]
+      public_subnets   = ["10.1.3.0/24", "10.1.4.0/24"]
+    }
+    prod = {
+      cidr             = "10.2.0.0/16"
+      private_subnets  = ["10.2.1.0/24", "10.2.2.0/24"]
+      public_subnets   = ["10.2.3.0/24", "10.2.4.0/24"]
+    }
+  }
+}
+
 # VPC Creation
 module "vpc" {
+  for_each = local.environments
+
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "eks-simple-vpc"
-  cidr = "10.0.0.0/16"
+  name = "${each.key}-eks-simple-vpc"
+  cidr = each.value.cidr
 
   azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets = each.value.private_subnets
+  public_subnets  = each.value.public_subnets
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -19,13 +41,15 @@ module "vpc" {
 
 # EKS Cluster
 module "eks" {
+  for_each = local.environments
+
   source = "terraform-aws-modules/eks/aws"
 
-  name               = var.cluster_name
+  name               = "${var.cluster_name}-${each.key}"
   kubernetes_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc[each.key].vpc_id
+  subnet_ids = module.vpc[each.key].private_subnets
 
   eks_managed_node_groups = {
     default = {
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "cluster_names" {
+  description = "Map of environment to EKS cluster name"
+  value       = { for env, eks_module in module.eks : env => eks_module.cluster_name }
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+output "cluster_endpoints" {
+  description = "Map of environment to EKS control plane endpoint"
+  value       = { for env, eks_module in module.eks : env => eks_module.cluster_endpoint }
 }
